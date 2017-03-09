5c6ba79566581109aa78dd9072a553bd48f2860e
josezero0 <josedavid.pc@outlook.com>
2017-03-09 17:31:20 +0100

    Add files via upload

--- Form-Exam/public_html/js/index.js
+++ Form-Exam/public_html/js/index.js
@@ -1,10 +1,1 @@
-/**
- * Al cargar la página...
- * 1. Al hacer click en el botón 'empezar' se irá a la página del exeman.
- * @returns {undefined}
- */
-window.onload = function () {
-    document.getElementsByClassName("button")[0].onclick = function () {
-        window.location.href = "exam.html";
-    };
-};
+window.onload=function(){document.getElementsByClassName("button")[0].onclick=function(){window.location.href="exam.html"}};
